c20c33481751d87900ff961a1cdb0faad31a0436
Chris Thornton <ctthornt@gmail.com>
2019-06-20 00:31:08 -0700

    add base app and navigation bar

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="navHeader">
+        <div className="navLeft">
+          <h2>Home</h2>
+          <h2>My Account</h2>
+          <h2>Recruit Volunteers</h2>
+          <h2 className="navRight">Login / Signup</h2>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
